ff07a7e21c0bed8e6fde541bbcbb94b32ee96765
hoyeoy <hoyeonyang65@gmail.com>
2025-11-06 08:48:20 +0900

    1106 3

--- api/index4.py
+++ api/index4.py
@@ -13,11 +13,11 @@ app = Flask(__name__)
 # ===============================
 # 🔧 기본 설정
 # ===============================
-# YESTERDAY = (datetime.now(timezone('Asia/Seoul')) - timedelta(days=1)).strftime('%Y.%m.%d')
-# TODAY = datetime.now().strftime('%Y.%m.%d')
-ESTERDAY = (datetime.now() - timedelta(hours=24)).strftime('%Y.%m.%d')
+YESTERDAY = (datetime.now(timezone('Asia/Seoul')) - timedelta(hours=24)).strftime('%Y.%m.%d')
+TODAY = datetime.now(timezone('Asia/Seoul')).strftime('%Y.%m.%d')
+"""YESTERDAY = (datetime.now() - timedelta(hours=24)).strftime('%Y.%m.%d')
 TODAY = datetime.now().strftime('%Y.%m.%d')
-
+"""
 HEADERS = {
     "User-Agent": (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
