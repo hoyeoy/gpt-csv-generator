1837fd4e132d490a3a2683169377f9b556b787b1
hoyeoy <hoyeonyang65@gmail.com>
2025-11-06 08:59:41 +0900

    1106 04

--- api/index4.py
+++ api/index4.py
@@ -85,7 +85,7 @@ def get_todays_investchosun_news():
 
                 date_text = date_span.get_text(strip=True).strip()
 
-                if date_text != YESTERDAY:
+                if date_text != (YESTERDAY or TODAY):
                     continue
 
                 if full_url in urls:
